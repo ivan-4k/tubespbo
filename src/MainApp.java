58180047b067e969b2b9c6283250dea7df247277
suliisswatun <suliswatunhasanah1@gmail.com>
2025-12-26 12:05:02 +0700

    tambahan dimain

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,14 +1,24 @@
-
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
+// atau: import com.formdev.flatlaf.FlatDarkLaf;
+
 import view.AntrianKlinikFrame;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            // UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(() -> {
             new AntrianKlinikFrame().setVisible(true);
         });
-
     }
 }
